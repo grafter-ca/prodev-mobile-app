4c8ac24b4289c7d431e133db87b39246d0269d99
habyarimana caleb <callebhabyar55@gmail.com>
2025-09-04 01:28:40 +0200

    add _layout file

---
+++ prodev-mobile-app-0x05/app/(home)/_layout.tsx
@@ -0,0 +1,12 @@
+// app/(home)/_layout.tsx
+import { Stack } from "expo-router";
+
+export default function HomeLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false, // Hide headers if you want a custom design
+      }}
+    />
+  );
+}
